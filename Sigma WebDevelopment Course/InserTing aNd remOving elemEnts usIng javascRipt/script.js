afc5c45c66c12a3d59a525e9c1c0d08380e38ac9
Ayush-Dobariya <ayushdobariya2024@gmail.com>
2025-03-23 10:45:44 +0530

    Add files via upload

---
+++ Sigma WebDevelopment Course/InserTing aNd remOving elemEnts usIng javascRipt/script.js
@@ -0,0 +1,16 @@
+// document.querySelector(".container").innerHTML = "Hello, I am a Jasprit Bumrah"
+// console.dir(document.querySelector(".container").getAttribute("style"))
+// let div = document.createElement("div");
+
+
+
+
+// let cint = document.querySelector(".container")
+// cint.insertAdjacentHTML("afterend", "<b>Helo i am fine</b>")
+// cint.remove()
+
+
+
+
+
+console.log(document.querySelector(".container").className)
